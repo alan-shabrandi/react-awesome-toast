391ba9a7e65210ffe1b8bf76f61132462449b673
alan.shabrandi <alan.shabrandi@gmail.com>
2023-06-17 17:54:29 +0330

    [BUGFIX] undo button

--- src/reactAwesomeToast/components/undo.tsx
+++ src/reactAwesomeToast/components/undo.tsx
@@ -5,28 +5,26 @@ import { toast } from "../ReactToast";
 
 const Undo = ({toastInfo, callbackTitle, callbackFunction, theme, toasts, setToasts}: UndoProps) => {
 
-    const [isClick, setIsClick] = useState<boolean>(false);
+    const [isClicked, setIsClicked] = useState<boolean>(false);
 
     const handleCallbackFunction = () => {
-        if(!isClick) {
-            setIsClick(true)
-            const promise = callbackFunction();
-            if (promise) {
-                toast({ title: "در حال بازگردانی", type: "promise" });
-                promise.then(() => {
-                    toast({ ...toastInfo, title: "عملیات بازگردانده شد.", type: toastInfo.type ?? "success", callbackFunction:null});
-                }).catch(() => {
-                    toast({ ...toastInfo, title: "خطا در انجام عملیات", type: "error", callbackFunction: null});
-                }).finally(() => {
-                    const filteredToasts = toasts.filter((toast: ToastProps) => toast.type !== "promise");
-                    setToasts(filteredToasts);
-                });
-            }
+        const promise = callbackFunction();
+        if (promise && !isClicked) {
+            setIsClicked(true);
+            toast({ title: "در حال بازگردانی", type: "promise" });
+            promise.then(() => {
+                toast({ ...toastInfo, title: "عملیات بازگردانده شد.", type: toastInfo.type ?? "success", callbackFunction:null});
+            }).catch(() => {
+                toast({ ...toastInfo, title: "خطا در انجام عملیات", type: "error", callbackFunction: null});
+            }).finally(() => {
+                const filteredToasts = toasts.filter((toast: ToastProps) => toast.type !== "promise");
+                setToasts(filteredToasts);
+            });
         }
     }
 
     return (
-        <div className={`toast_undo ${theme === "dark" ? "toast_undo-dark" : "toast_undo-light"}`} onClick={handleCallbackFunction}>
+        <div className={`toast_undo ${theme === "dark" ? "toast_undo-dark" : "toast_undo-light"}`} style={{cursor: isClicked ? "default" : "pointer"}} onClick={handleCallbackFunction}>
             <UndoIcon theme={theme}/>
             <span>{callbackTitle}</span>
         </div>
